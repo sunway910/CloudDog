4887e92ddc2733aec790a8a64538d86301f5c086
0utsiderZhong <1092280043@q.com>
2023-08-10 16:25:33 +0800

    init vue template

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,13 +1,13 @@
 """backend URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to pages. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
+Function pages
+    1. Add an import:  from my_app import pages
+    2. Add a URL to urlpatterns:  path('', pages.home, name='home')
+Class-based pages
+    1. Add an import:  from other_app.pages import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
--- tov-template/src/plugins/pinia.ts
+++
@@ -1,7 +0,0 @@
-import persistedstate from 'pinia-plugin-persistedstate'
-
-const pinia = createPinia()
-
-pinia.use(persistedstate)
-
-export default pinia
