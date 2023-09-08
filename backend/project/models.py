e3687a91e3f1a844cf00da5488f4a372f6aaa9ea
维维维 <1092280043@qq.com>
2023-09-08 08:58:03 +0800

    modify column default length and add dependency

--- backend/project/models.py
+++ backend/project/models.py
@@ -21,11 +21,11 @@ class Project(models.Model):
     id = models.AutoField(primary_key=True, db_comment='主键ID')
     cloud_platform = models.CharField(default='AlibabaCloud', max_length=100, verbose_name='CloudPlatform', db_comment='云平台类型', choices=CloudPlatform)
     account = ArrayField(models.CharField(), default=list, verbose_name='Account', db_comment='项目RAM账号')
-    project_name = models.CharField(default='港铁MTR', max_length=100, verbose_name='ProjectName', db_comment='项目名称')
+    project_name = models.CharField(default='MTR', max_length=500, verbose_name='ProjectName', db_comment='项目名称')
     project_access_key = models.CharField(default='', max_length=100, verbose_name='AK', db_comment='Access Key')
     project_secret_key = models.CharField(default='', max_length=100, verbose_name='SK', db_comment='Secret Key')
-    key_authority = models.CharField(default='ReadOnlyAccess', max_length=100, verbose_name='KeyAuthority', db_comment='Key的权限')
-    status = models.CharField(default='Running', max_length=100, verbose_name='ProjectStatus', db_comment='项目状态', choices=Status)
+    key_authority = models.CharField(default='ReadOnlyAccess', max_length=50, verbose_name='KeyAuthority', db_comment='Key的权限')
+    status = models.CharField(default='Running', max_length=30, verbose_name='ProjectStatus', db_comment='项目状态', choices=Status)
     create_time = models.DateField(default=timezone.now, verbose_name='CreateTime', db_comment='账号创建时间')
 
     def __str__(self):
