c8050d7e5fd56da48e074d50abc6b1b752c3b27b
0utsiderZhong <1092280043@qq.com>
2023-09-24 23:34:17 +0800

    promote logic when page change

--- frontend/src/plugins/router.ts
+++ frontend/src/plugins/router.ts
@@ -1,11 +1,10 @@
-import { createGetRoutes, setupLayouts } from 'virtual:meta-layouts'
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import { routes as fileRoutes } from 'vue-router/auto/routes'
-import { useAuthStore } from '@/stores/auth'
-import { reactive } from 'vue'
+import {createGetRoutes, setupLayouts} from 'virtual:meta-layouts'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import {routes as fileRoutes} from 'vue-router/auto/routes'
+import {useAuthStore} from '@/stores/auth'
+import {reactive} from 'vue'
 
 declare module 'vue-router' {
-	// 在这里定义你的 meta 类型
 	interface RouteMeta {
 		name?: string
 		title?: string
@@ -63,35 +62,35 @@ export const router = createRouter({
 	history: createWebHistory(),
 	routes: setupLayouts(custom_layout_route_list),
 })
-import { ref } from 'vue'
+
 
 export const getRoutes = createGetRoutes(router)
-// interface tokenVerify {
-//   token: any;
-// }
-const verify = reactive({ token: '' })
+
+const expiredTime = localStorage.getItem('expiredTime')
 
 router.beforeEach((to, from, next) => {
-    document.title = `${to.meta.title}` != null ? `${to.meta.title}` : 'CloudDog'
-    const auth = useAuthStore()
-    let now_time = new Date().getTime()
-    let expire_time: number
-    if (localStorage.getItem("access") != null) {
-        if (expiredTime != null) {
-            expire_time = parseInt(expiredTime)
-        } else {
-            expire_time = -1
-        }
-    } else {
-        expire_time = -1
-    }
-    if (now_time > expire_time && to.path !== '/login') {
-        next('/login')
-    } else if (to.meta.permiss && !auth.key.includes(to.meta.permiss)) {
-        next('/404')
-    } else {
-        next()
-    }
+	document.title = `${to.meta.title}` != null ? `${to.meta.title}` : 'CloudDog'
+	const auth = useAuthStore()
+	let token = localStorage.getItem("access")
+	let now_time = new Date().getTime()
+	let expire_time: number = -1
+	if (token != null) {
+		if (expiredTime != null) {
+			expire_time = parseInt(expiredTime)
+			if (now_time > expire_time && to.path !== '/login') {
+				next('/login')
+			} else if (to.meta.permiss && !auth.key.includes(to.meta.permiss)) {
+				next('/404')
+			} else {
+				next()
+			}
+		} else {
+			next('/login')
+		}
+	} else {
+		next('/login')
+	}
+
 })
 
 export default router
